0660454c3e7f00113622d05b013046d475ce0045
Chun Wu <the.chun.wu@gmail.com>
2024-01-20 23:55:04 -0800

    Functional basic js_webrtc video chat.

--- README.md
+++ README.md
@@ -7,16 +7,13 @@ This repository will build a 1-to-1 video chat, where each peer streams directly
 ### Peer 1
 1. Start a webcam feed
 2. Create a "RTPeerConnection" connection
-3. CAll 'createOffer()' and write the offer to the database.
-4. Listen to the database for an answer
-5. Share ICE candidates with one peer
-6. Show remote video feed.
+3. Call 'createOffer()' generates the offer in the textbox.
+4. Copy the offer to the opposing browser and paste it into the SDP offer textbox.
+ Share ICE candidates with one peer
+6. Paste answer to SDP Answer and create Add Answer.
+7. Both video feeds should now be live.
 
 ### Peer 2
 1. Start a webcam feed
 2. Create an 'RTCPeerConnection' connection
-3. Fetch database document with the offer.
-4. Call `createAnswer()`, then write to database.
-5. Share ICE candidates with other peer.
-6. Show remote video feed.
-
+5. Create an answer SDP off of Peer #1s SDP. Copy the SDP answer and paste back into browser #1
--- main.js
+++ main.js
@@ -1,183 +1,82 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+let peerConnection;
+let localStream;
+let remoteStream;
 
-import 'firebase/firestore'
-import firebase from 'firebase/app'
-import './style.css'
+let servers = {
+    iceServers : [
+        {
+            urls:['stun:stun1.1.google.com:19302','stun:stun2.1.google.com:19302']
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyANOvCns6pMinKSIKn1NFrN4btEmgGeAVA",
-  authDomain: "jswebrtc-d6e84.firebaseapp.com",
-  projectId: "jswebrtc-d6e84",
-  storageBucket: "jswebrtc-d6e84.appspot.com",
-  messagingSenderId: "123551634511",
-  appId: "1:123551634511:web:8d53ef4855c76c1dcfc63a",
-  measurementId: "G-QY0GP144V9"
-};
+        }
+    ]
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const firestore = firebase.firestore();
+let init = async() => {
+    localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
 
-// Adds the STUN server, where the data transfer.
-// Free iceServers from Google.
-const servers = {
-    iceServers: [
-      {
-        urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'], // free stun server
-      },
-    ],
-    iceCandidatePoolSize: 10,
-};
+    document.getElementById('user-1').srcObject = localStream
+}
 
-const peerConnection = new RTCPeerConnection(servers);
-let localStream = null;
-let remoteStream = null;
+let createPeerConnection = async (sdpType) => {
+    peerConnection = new RTCPeerConnection(servers)
 
-// Access Elements in HTML using Document Object Model (DOM)
-const webcamButton = document.getElementById('webcamButton');
-const webcamVideo = document.getElementById('webcamVideo');
-const callButton = document.getElementById('callButton');
-const callInput = document.getElementById('callInput');
-const answerButton = document.getElementById('answerButton');
-const remoteVideo = document.getElementById('remoteVideo');
-const hangupButton = document.getElementById('hangupButton');
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
 
-webcamButton.onclick = async () => {
-
-    // setting local stream to the video from our camera
-    localStream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream)
     })
 
-    // initalizing the remote server to the mediastream
-    remoteStream = new MediaStream();
-
-
-    // Pushing tracks from local stream to peerConnection
-    localStream.getTracks().forEach(track => {
-        pc.addTrack(track, localStream);
-    })
-
-    pc.ontrack = event => {
-        event.streams[0].getTracks(track => {
+    peerConnection.ontrack = async (event) => {
+        event.streams[0].getTracks().forEach((track) => {
             remoteStream.addTrack(track)
         })
-    }  
+    } 
 
-    // displaying the video data from the stream to the webpage
-    webcamVideo.srcObject = localStream;
-    remoteVideo.srcObject = remoteStream;
-
-    // enabling and disabling interface based on the current condtion
-    callButton.disabled = false;
-    answerButton.disabled = false;
-    webcamButton.disabled = true;
-}
-
-callButton.onclick = async () => {
-
-    // referencing firebase collections
-    const callDoc = firestore.collection('calls').doc();
-    const offerCandidates = callDoc.collection('offerCandidates');
-    const answerCandidiates = callDoc.collection('answerCandidates');
-
-    // setting the input value to the calldoc id
-    callInput.value = callDoc.id;
-
-    // get candidiates for caller and save to db
-    pc.onicecandidate = event => {
-        event.candidate && offerCandidates.add(event.candidate.toJSON());
-    }
-
-    // create offer
-    const offerDescription = await pc.createOffer();
-    await pc.setLocalDescription(offerDescription);
-
-    // config for offer
-    const offer = {
-        sdp: offerDescription.sdp,
-        type: offerDescription.type
-    }
-
-    await callDoc.set({offer});
-
-    
-    // listening to changes in firestore and update the streams accordingly
-
-    callDoc.onSnapshot(snapshot => {
-        const data = snapshot.data();
-
-        if (!pc.currentRemoteDescription && data.answer) {
-            const answerDescription = new RTCSessionDescription(data.answer);
-            pc.setRemoteDescription(answerDescription);
+    peerConnection.onicecandidate = async (event) => {
+        if (event.candidate) {
+            document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription)
         }
+    }
+}
 
-        // if answered add candidates to peer connection
-        answerCandidiates.onSnapshot(snapshot => {
-            snapshot.docChanges().forEach(change => {
+let createOffer = async () => {
+    createPeerConnection('offer-sdp')
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
 
-                if (change.type === 'added') {
-                    const candidate = new RTCIceCandidate(change.doc.data());
-                    pc.addIceCandidate(candidate);
-                }
-            })
-        })
-    })
+    document.getElementById('offer-sdp').value = JSON.stringify(offer)
+}
 
-    hangupButton.disabled = false;
+let createAnswer = async () => {
+    createPeerConnection('answer-sdp')
+
+    let offer = document.getElementById('offer-sdp').value
+    if (!offer) return alert('Retrieve offer from peer first...')
+
+    offer = JSON.parse(offer)
+    await peerConnection.setRemoteDescription(offer)
+
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    document.getElementById('answer-sdp').value = JSON.stringify(answer)
+}
+
+let addAnswer = async() => {
+    let answer = document.getElementById('answer-sdp').value
+    if (!answer) return alert('Retrieve answer from peer first...')
+
+    answer = JSON.parse(answer)
+
+    if (!peerConnection.currentRemoteDescription) {
+        peerConnection.setRemoteDescription(answer)
+    }
 
 }
 
-answerButton.onclick = async () => {
-    const callId = callInput.value;
-  
-    // getting the data for this particular call
-    const callDoc = firestore.collection('calls').doc(callId); 
-                    
-    const answerCandidates = callDoc.collection('answerCandidates');
-    const offerCandidates = callDoc.collection('offerCandidates');
+init()
 
-    // here we listen to the changes and add it to the answerCandidates
-    pc.onicecandidate = event => {
-        event.candidate && answerCandidates.add(event.candidate.toJSON());
-
-    }
-
-    const callData = (await callDoc.get()).data();
-
-    // setting the remote video with offerDescription
-    const offerDescription = callData.offer;
-    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
-    
-
-    // setting the local video as the answer
-    const answerDescription = await pc.createAnswer();
-    await pc.setLocalDescription(new RTCSessionDescription(answerDescription));
-
-    // answer config
-    const answer = {
-        type: answerDescription.type,
-        sdp: answerDescription.sdp
-    }
-
-    await callDoc.update({ answer });
-
-    offerCandidates.onSnapshot(snapshot => {
-        snapshot.docChanges().forEach(change => {
-
-            if (change.type === 'added') {
-                let data = change.doc.data();
-                pc.addIceCandidate(new RTCIceCandidate(data));
-
-            }
-        })
-    })
-}
+document.getElementById('create-offer').addEventListener('click', createOffer)
+document.getElementById('create-answer').addEventListener('click', createAnswer)
+document.getElementById('add-answer').addEventListener('click', addAnswer)
